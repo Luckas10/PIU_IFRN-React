77d38066094f9e577775d7a024c226ee7c4e9b18
JOÃO LUCAS GOMES DE SOUZA <20221101110040@ifrn.local>
2025-05-07 14:24:01 -0300

    First class

---
+++ Aula01/src/MyFirstComponent.jsx
@@ -0,0 +1,17 @@
+function MyFirstComponent() {
+
+    const name = "João Lucas";
+
+    return(
+        <>
+        <div>
+            <h1>Oi cambada!!!! Deixa o like e se inscreva no canal</h1>
+        </div>
+        <div>
+            <p>Seu nome é {name}</p>
+        </div>
+        </>
+    );
+}
+
+export default MyFirstComponent
