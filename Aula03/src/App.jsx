b40aa7cb5d8787747f8a90b645ee8b541ee9e6db
JOÃO LUCAS GOMES DE SOUZA <20221101110040@ifrn.local>
2025-05-21 14:18:28 -0300

    add aula 4

--- Aula03/src/App.jsx
+++ Aula03/src/App.jsx
@@ -1,14 +1,48 @@
 import './App.css'
 import Info1V from './components/Info1V'
+import Card from './components/Card'
+import ScopeReduces from './components/ScopeReduces'
+import { useState } from 'react'
 
 function App() {
 
+  const maximo = 3
+  const minimo = 1
+  const [cont, setCont] = useState(minimo)
+
+
+  function avancar() {
+    if (cont < maximo) {
+      setCont(cont + 1)
+    }
+  }
+
+  function voltar() {
+    if (cont > minimo) {
+      setCont(cont - 1)
+    }
+  }
+
+  let content;
+  if (cont === 1) {
+    content = <Info1V />
+  } else if (cont === 2) {
+    content = <Card />
+  } else if (cont === 3) {
+    content = <ScopeReduces />
+  }
+
   return (
-    <>
-      {/* <Info1V /> */}
+    <div className='body'>
       <h1>Galeria de componentes</h1>
-      <section className='screen'></section>
-    </>
+      <section className='screen'>
+        {content}
+      </section>
+      <div className="buttons">
+        <button onClick={voltar} className='btn btnVoltar'>⬅</button>
+        <button onClick={avancar} className='btn btnAvancar'>➡</button>
+      </div>
+    </div>
   )
 }
 
