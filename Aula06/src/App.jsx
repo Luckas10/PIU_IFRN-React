a953f9a90f6388dc29a8c19e26764c3473bf1da8
JOÃO LUCAS GOMES DE SOUZA <20221101110040@ifrn.local>
2025-07-16 14:27:35 -0300

    Add aula06

---
+++ Aula06/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import CountClicks from './components/CountClicks'
+import EffectAPI from './components/EffectApi'
+import RepoJson from './components/RepoJson'
+
+
+function App() {
+
+  return (
+    <>
+      {/* <CountClicks/> */}
+      <EffectAPI /> 
+      {/* <RepoJson /> */}
+      
+    </>
+  )
+}
+
+export default App
---
+++ Aula06/src/components/EffectAPI.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+
+export default function EffectAPI() {
+    const [usuarios, setUsuarios] = useState([]);
+    const [loading, setLoading] = useState(false); // Estado de carregamento
+    const [API, setAPI] = useState(false);
+
+    useEffect(() => {
+        if (!API) return;
+
+        // Usamos uma função assíncrona dentro do useEffect
+        const fetchUsuarios = async () => {
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/users'); //fecth faz uma requisição HTTP utilizando o método GET
+                const data = await response.json(); //await faz com que a requisição aguarde os dados chegarem. O método json converte os dados para json
+                setUsuarios(data); // Atualiza o estado com os dados recebidos
+            } catch (error) {
+                console.error("Erro ao buscar usuários:", error);
+            } finally {
+                setLoading(false); // Finaliza o carregamento
+                setAPI(false)
+            }
+        }
+
+        fetchUsuarios();
+    }, [{API}]); // O array vazio garante que o efeito execute apenas uma vez
+
+    if (loading) {
+        return <p>Carregando...</p>;
+    }
+
+    function ChamarAPI() {
+        setAPI(true)
+    }
+
+    return (
+        <div>
+            <h1>Lista de Usuários</h1>
+            <button onClick={ChamarAPI}>Chamar API</button>
+            <ul>
+                {usuarios.map(usuario => (
+                    <li key={usuario.id}>{usuario.name}</li>
+                    
+                ))}
+            </ul>    
+        </div>
+    );
+}
